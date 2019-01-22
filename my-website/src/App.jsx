acce26e3a7c5f364064ce1413a92c8f715013ad0
iBasel <basel.e@me.com>
2019-01-21 20:14:05 -0500

    react

---
+++ my-website/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>Basel Abdelaziz - Software Engineer</div>
+        <div>basel.e@me.com</div>
+      </div>
+    );
+  }
+}
+
+export default App;
