35546631116329355ce13f7cbc358b61366bd7c9
iBasel <basel.e@me.com>
2019-01-24 19:10:17 -0500

    portfolio images

---
+++ precache-manifest.7e1d758b77da61b91ffdd1c905d17b54.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "06a4314f6bb33af6d17d4fc9ec786fa5",
+    "url": "/ibasel.github.io/static/media/6.06a4314f.png"
+  },
+  {
+    "revision": "de5333ac90142bea991b",
+    "url": "/ibasel.github.io/static/css/main.5c6fa643.chunk.css"
+  },
+  {
+    "revision": "0f14a056098304ba7cdb",
+    "url": "/ibasel.github.io/static/js/1.0f14a056.chunk.js"
+  },
+  {
+    "revision": "caa5c46536f7d448de53",
+    "url": "/ibasel.github.io/static/js/runtime~main.caa5c465.js"
+  },
+  {
+    "revision": "ea5d0230f0a3f1d5c4762674aead9446",
+    "url": "/ibasel.github.io/static/media/1.ea5d0230.png"
+  },
+  {
+    "revision": "6a9b1ebbd10bf81def7c5d5ce8eef5ca",
+    "url": "/ibasel.github.io/static/media/2.6a9b1ebb.png"
+  },
+  {
+    "revision": "3723851bf5833815c837b49131aae68a",
+    "url": "/ibasel.github.io/static/media/3.3723851b.png"
+  },
+  {
+    "revision": "10aa6e8527807c421f7c61846b12d122",
+    "url": "/ibasel.github.io/static/media/4.10aa6e85.png"
+  },
+  {
+    "revision": "5962f6fa3e5f1d036f92d2d1888cc505",
+    "url": "/ibasel.github.io/static/media/5.5962f6fa.png"
+  },
+  {
+    "revision": "de5333ac90142bea991b",
+    "url": "/ibasel.github.io/static/js/main.de5333ac.chunk.js"
+  },
+  {
+    "revision": "00ca0adddecc3b471aca10faf34168ec",
+    "url": "/ibasel.github.io/static/media/7.00ca0add.png"
+  },
+  {
+    "revision": "4b46a2e1aaf0b7dccfb46f14dd8dc3d7",
+    "url": "/ibasel.github.io/static/media/8.4b46a2e1.png"
+  },
+  {
+    "revision": "2e09f8f59ce886a23e70da7148cdfa48",
+    "url": "/ibasel.github.io/static/media/9.2e09f8f5.png"
+  },
+  {
+    "revision": "65849bdb4e5a2a0ff58aafd69abed1f4",
+    "url": "/ibasel.github.io/static/media/unity.65849bdb.png"
+  },
+  {
+    "revision": "9d44bb8b2952a1638dcc21467b34a4f3",
+    "url": "/ibasel.github.io/static/media/react.9d44bb8b.png"
+  },
+  {
+    "revision": "cd8c1b43f2b0ae725e1a925c4038ed2e",
+    "url": "/ibasel.github.io/static/media/xcode.cd8c1b43.png"
+  },
+  {
+    "revision": "05f946431719cf7a4538f35a7ddd65f7",
+    "url": "/ibasel.github.io/static/media/start-page.05f94643.jpg"
+  },
+  {
+    "revision": "8ff113b2f5327d8aae8822a328d8e158",
+    "url": "/ibasel.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ibasel.github.io/precache-manifest.5651b744a4195b071e9d858888d04fd8.js"
+  "/ibasel.github.io/precache-manifest.7e1d758b77da61b91ffdd1c905d17b54.js"
 );
 
 workbox.clientsClaim();
